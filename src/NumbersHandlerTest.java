e7b30dd3bc57bc7dc2fc258b618c9a2152066bb9
ValidolAndreevich <deadpool1725@tut.by>
2021-10-05 15:27:48 +0300

    Add test for NumberHandler class.

---
+++ src/NumbersHandlerTest.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumbersHandlerTest {
+
+    NumbersHandler handler = new NumbersHandler();
+
+    @Test
+    void averageMethodGetsEmptyList() {
+        double expResult = 0;
+        double actResult = handler.findAverageCurrent();
+        assertEquals(expResult,actResult);
+    }
+
+    @Test
+    void averageMethodGetsNotEmptyList() {
+        handler.addNumberToList("0");
+        handler.addNumberToList("3");
+        handler.addNumberToList("-7");
+        handler.addNumberToList("2.5");
+        handler.addNumberToList("-1.5");
+        double expResult = -0.6;
+        double actResult = handler.findAverageCurrent();
+        assertEquals(expResult,actResult);
+    }
+
+    @Test
+    void getLargestCurrentNumber() {
+        handler.addNumberToList("-32");
+        handler.addNumberToList("-32.001");
+        handler.addNumberToList("0");
+        handler.addNumberToList("47");
+        handler.addNumberToList("47.001");
+        double expResult = 47.001;
+        double actResult = handler.getLargestCurrentNumber();
+        assertEquals(expResult,actResult);
+    }
+
+    @Test
+    void getSmallestCurrentNumber() {
+        handler.addNumberToList("-1.001");
+        handler.addNumberToList("-0.001");
+        handler.addNumberToList("0");
+        handler.addNumberToList("0.5");
+        handler.addNumberToList("17.001");
+        double expResult = -1.001;
+        double actResult = handler.getSmallestCurrentNumber();
+        assertEquals(expResult,actResult);
+    }
+
+    @Test()
+    void exceptionOnStringAdd() {
+        String input = "string value";
+        assertDoesNotThrow(() -> {handler.addNumberToList(input);});
+    }
+}
